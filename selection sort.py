ac7ff1bb9a2c16b66727f6ea4919e4c322929bb8
Tushar Patel <118635170+tusharrrrrp@users.noreply.github.com>
2024-04-25 00:36:53 +0530

    Update selection sort.py

--- selection sort.py
+++ selection sort.py
@@ -1,20 +1,15 @@
-# Selection sort in Python
-# time complexity O(n*n)
-# sorting by finding min_index
-def selectionSort(array, size):
-    for ind in range(size):
-        min_index = ind
-
-        for j in range(ind + 1, size):
-            # select the minimum element in every iteration
-            if array[j] < array[min_inde x]:
-                min_index = j
-        # swapping the elements to sort the array
-        (array[ind], array[min_index]) = (array[min_index], array[ind])
-
-
-arr = [-2, 45, 0, 11, -9, 88, -97, -202, 747]
-size = len(arr)
-selectionSort(arr, size)
-print('The array after sorting in Ascending Order by selection sort is:')
-print(arr)
+def selection_sort(arr,size):
+    n=len(arr)
+    if n<=1:
+        return
+    for i in range ( size ):   
+         key = i
+         for j in range( i +1 , size ):
+             if arr[j]<key:
+                 key=j
+         (arr[i],arr[key] )= (arr[key],arr[i])
+if __name__=="__main__":
+    arr=[-2, 45, 0, 11, -9, 88, -97, -202, 747]
+    size=len(arr)
+    selection_sort(arr,size)
+    print(arr)
